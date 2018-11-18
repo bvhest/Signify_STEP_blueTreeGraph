4cac5ce004336a68966e01ddb0d8112024f3e3e2
b van hest <hestbv@gmail.com>
2018-11-18 22:27:24 +0100

    neo4j import script werkt op test-set

--- code/10_conv_xml2csv.sh
+++ code/10_conv_xml2csv.sh
@@ -1,9 +1,15 @@
-#!/bin/bash
+##!/bin/bash
 #
+# Signify STEP product database
+#
+# script om datamodel op te zetten en data te laden in de Neo4j database
 # create neo4j database with Signify Product tree and mandatory attributes from csv-data:
+#
+# BvH, 2018-11-15
+#
 
 # product blue tree to category-nodes, including category links :
-#java -cp /home/hestbv/Tools/Saxon/saxon9he.jar net.sf.saxon.Transform -t -s:../data/raw/prod_blue_tree.xml -xsl:10_prod_blue_tree_xml2csv.xsl -o:../data/raw/prod_blue_tree.csv
+java -cp /home/hestbv/Tools/Saxon/saxon9he.jar net.sf.saxon.Transform -t -s:../data/raw/prod_blue_tree.xml -xsl:10_prod_blue_tree_xml2csv.xsl -o:../data/raw/prod_blue_tree.csv
 
 # attribute definitions to attribute-nodes, including links to product blue-tree and links that are mandatory:
 java -cp /home/hestbv/Tools/Saxon/saxon9he.jar net.sf.saxon.Transform -t -s:../data/raw/attributes.xml -xsl:12_attributes_xml2csv.xsl -o:../data/raw/attributes.csv
--- code/20_loadNeo4j.sh
+++ code/20_loadNeo4j.sh
@@ -1,1 +1,20 @@
-/home/hestbv/Tools/neo4j-community-3.2.0/bin/neo4j-shell -path /home/hestbv/Tools/neo4j-community-3.2.0/data/graph.db -config /home/hestbv/Tools/neo4j-community-3.2.0/conf/neo4j.properties -file 20_neo4j-importCSV.cql
+#!/bin/bash
+#
+# Signify STEP product database
+#
+# script om datamodel op te zetten en data te laden in de Neo4j database
+#
+# BvH, 2018-11-15
+#
+
+# Check if a directory does not exist:
+if [ ! -d "/home/hestbv/Tools/neo4j-community-3.2.0/data/databases/graph.db/import" ] 
+then
+  mkdir /home/hestbv/Tools/neo4j-community-3.2.0/data/databases/graph.db/import
+fi
+# cp data-files to Neo4j import-directory:
+cp /home/hestbv/Projecten/Neo4j/Signify/data/raw/prod_blue_tree.csv /home/hestbv/Tools/neo4j-community-3.2.0/data/databases/graph.db/import/prod_blue_tree_test.csv
+cp /home/hestbv/Projecten/Neo4j/Signify/data/raw/attributes.csv /home/hestbv/Tools/neo4j-community-3.2.0/data/databases/graph.db/import/attributes_test.csv
+
+# load data:
+/home/hestbv/Tools/neo4j-community-3.2.0/bin/neo4j-shell -path /home/hestbv/Tools/neo4j-community-3.2.0/data/databases/graph.db -config /home/hestbv/Tools/neo4j-community-3.2.0/conf/neo4j.conf -file 20_neo4j-importCSV.cql

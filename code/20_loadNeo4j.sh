94b9982eba894596601db028284027bd6e0a6cfa
B van Hest <hestbv@gmail.com>
2019-03-25 22:33:41 +0100

    Better data, completer import, better queries. Mandatory product specifications per nav.tree category can now be queried!

--- code/20_loadNeo4j.sh
+++ code/20_loadNeo4j.sh
@@ -14,9 +14,10 @@ then
   mkdir /home/hestbv/Tools/neo4j-community-3.2.0/data/databases/graph.db/import
 fi
 # cp data-files to Neo4j import-directory:
-mv /home/hestbv/Projecten/Neo4j/Signify/data/csv/nav_yellow_tree.csv /home/hestbv/Tools/neo4j-community-3.2.0/data/databases/graph.db/import/nav_yellow_tree.csv
-mv /home/hestbv/Projecten/Neo4j/Signify/data/csv/prod_blue_tree.csv /home/hestbv/Tools/neo4j-community-3.2.0/data/databases/graph.db/import/prod_blue_tree.csv
-mv /home/hestbv/Projecten/Neo4j/Signify/data/csv/attributes.csv /home/hestbv/Tools/neo4j-community-3.2.0/data/databases/graph.db/import/attributes.csv
+mv /home/hestbv/Projecten/Neo4j/Signify_STEPdatamodel/data/csv/nav_yellow_tree.csv /home/hestbv/Tools/neo4j-community-3.2.0/data/databases/graph.db/import/nav_yellow_tree.csv
+mv /home/hestbv/Projecten/Neo4j/Signify_STEPdatamodel/data/csv/prod_blue_tree.csv /home/hestbv/Tools/neo4j-community-3.2.0/data/databases/graph.db/import/prod_blue_tree.csv
+mv /home/hestbv/Projecten/Neo4j/Signify_STEPdatamodel/data/csv/attributes.csv /home/hestbv/Tools/neo4j-community-3.2.0/data/databases/graph.db/import/attributes.csv
+mv /home/hestbv/Projecten/Neo4j/Signify_STEPdatamodel/data/csv/prod_assets.csv /home/hestbv/Tools/neo4j-community-3.2.0/data/databases/graph.db/import/prod_assets.csv
 
 # load data:
 /home/hestbv/Tools/neo4j-community-3.2.0/bin/neo4j-shell -path /home/hestbv/Tools/neo4j-community-3.2.0/data/databases/graph.db -config /home/hestbv/Tools/neo4j-community-3.2.0/conf/neo4j.conf -file 20_neo4j-importCSV.cql > databaseCreation.log
